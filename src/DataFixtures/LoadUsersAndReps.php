504f629b8ddc4a55b7d4c0b85786e407daa9691f
Ryan Weaver <ryan@thatsquality.com>
2019-11-17 15:28:24 +0100

    Voila! New application

---
+++ src/DataFixtures/LoadUsersAndReps.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
+use App\Entity\RepLog;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class LoadUsersAndReps extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $items = array_flip(RepLog::getThingsYouCanLiftChoices());
+
+        $names = array(
+            array('Brad', 'Kitt'),
+            array('Cat', 'Middleton'),
+            array('Cindy', 'Clawford'),
+            array('Diane', 'Kitten'),
+            array('Fuzz', 'Aldrin'),
+            array('Hunter S.', 'Thomcat'),
+            array('J.R.R', 'Tollkitten'),
+            array('Madelion', 'Albright'),
+            array('Meowly', 'Cyrus'),
+            array('Ron', 'Furgandy'),
+        );
+
+        foreach ($names as $name) {
+            $firstName = $name[0];
+            $lastName = $name[1];
+
+            $user = new User();
+            $email = sprintf('%s_%s', $firstName, $lastName);
+            $email = strtolower($email);
+            $email = str_replace(' ', '', $email);
+            $email = str_replace('.', '', $email);
+            $user->setEmail($email.'@example.com');
+
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'pumpup'
+            ));
+            $user->setFirstName($firstName);
+            $user->setLastName($lastName);
+            $manager->persist($user);
+
+            for ($j = 0; $j < rand(1, 5); $j++) {
+                $repLog = new RepLog();
+                $repLog->setUser($user);
+                $repLog->setReps(rand(1, 30));
+                $repLog->setItem(array_rand($items));
+                $manager->persist($repLog);
+            }
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @Assert\NotBlank
+     * @ORM\Column(name="first_name", type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @Assert\NotBlank
+     * @ORM\Column(name="last_name", type="string", length=255)
+     */
+    private $lastName;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getAvatar(): string
+    {
+        return 'http://thecatapi.com/api/images/get?format=src&type=gif&r='.rand(100, 999);
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(?string $firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(?string $lastName)
+    {
+        $this->lastName = $lastName;
+    }
+}
