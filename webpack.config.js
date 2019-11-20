71c473f4e5b38277fb2b41953d6d374afcd8df55
Ryan Weaver <ryan@thatsquality.com>
2019-11-20 07:58:44 +0100

    Step 46: Organization: move to assets/

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,9 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './public/assets/js/app.js')
-    .addEntry('rep_log', './public/assets/js/rep_log.js')
-    .addEntry('login', './public/assets/js/login.js')
+    .addEntry('app', './assets/js/app.js')
+    .addEntry('rep_log', './assets/js/rep_log.js')
+    .addEntry('login', './assets/js/login.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
