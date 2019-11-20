628bcdbcd695c7d0c80c5fa8f45960fe564e73ff
Ryan Weaver <ryan@thatsquality.com>
2019-11-20 07:58:49 +0100

    Step 74: Fetch: Set up getRepLogs()

--- assets/js/Components/RepLogApp.js
+++ assets/js/Components/RepLogApp.js
@@ -3,6 +3,7 @@
 import Helper from './RepLogAppHelper';
 import $ from 'jquery';
 import Routing from './Routing';
+import { getRepLogs } from '../api/rep_log';
 
     let HelperInstances = new WeakMap();
 
@@ -42,9 +43,7 @@ import Routing from './Routing';
         }
 
         loadRepLogs() {
-            $.ajax({
-                url: Routing.generate('rep_log_list'),
-            }).then(data => {
+            getRepLogs().then(data => {
                 for (let repLog of data.items) {
                     this._addRow(repLog);
                 }
