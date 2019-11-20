9f8aa42c7497269a15ac7be819ce9dcae2b69722
Ryan Weaver <ryan@thatsquality.com>
2019-11-20 07:58:43 +0100

    Step 39: Bad Plugins: autoProvidejQuery

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
