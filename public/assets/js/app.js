32809a7862a715a57ed490d95c1ec10a92169991
Ryan Weaver <ryan@thatsquality.com>
2019-11-20 07:58:42 +0100

    Step 36: Bad Plugins: install jquery-tags-input

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,8 +2,12 @@
 
 const $ = require('jquery');
 require('bootstrap');
+require('jquery-tags-input');
 
     $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip();
+
+        // just example code
+        $('#tags').tagsInput();
     });
 
