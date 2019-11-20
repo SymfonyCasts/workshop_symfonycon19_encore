a75289b05d913dda901e80a09c163a4b61f35d03
Ryan Weaver <ryan@thatsquality.com>
2019-11-20 07:58:39 +0100

    Step 19: Webpack: delete package, webpack.config.js

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    entry: './public/assets/js/RepLogApp.js',
-    output: {
-        filename: 'rep_log.js',
-        path: __dirname+'/public/build'
-    },
-    mode: 'development'
-};
